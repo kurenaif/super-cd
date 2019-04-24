8127f763cbedfdce7b13cd59eac5c89242145357
kurenaif <antyobido@gmail.com>
2019-04-24 23:51:56 +0900

    skim sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+extern crate skim;
+use skim::{Skim, SkimOptionsBuilder};
+use std::default::Default;
+use std::io::Cursor;
+
+pub fn main() {
+    let options = SkimOptionsBuilder::default()
+        .height(Some("50%"))
+        .multi(true)
+        .build()
+        .unwrap();
+
+    let input = "aaaaa\nbbbb\nccc".to_string();
+
+    let selected_items = Skim::run_with(&options, Some(Box::new(Cursor::new(input))))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(|| Vec::new());
+
+    for item in selected_items.iter() {
+        print!("{}: {}{}", item.get_index(), item.get_output_text(), "\n");
+    }
 }
